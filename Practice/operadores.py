6531927ccb3453fab4595bf32ae441eeef175e6d
AndresYakuza <andresramos123@outlook.com>
2025-06-12 21:06:49 -0500

    Introduccion a operadores.

---
+++ Practice/operadores.py
@@ -0,0 +1,42 @@
+# El operador ' == ', es para igualdad. 
+# Ejemplo practico 
+
+var = 1 
+print('Prueba #1', var == 1) # Valor esperado a retornar 'True'. 
+###########################################
+
+# El operador ' != ', seria lo contrario, desigualdad. 
+# Ejemplo practico 
+
+var1 = 1
+print('Prueba #2', var != var1) # Valor esperado a retornar 'False', ya que ambas variables si tienen el mismo valor. 
+###########################################
+
+# El operador ' >= ', mayor o igual que. 
+# Tambien se puede usar independiente ' > ', mayor que.
+# Ejemplo practico 
+
+print('Prueba #3',  23 >= 21) # Valor esperado a retornar 'True', ya que 23 es mayor que 21. 
+print('Prueba #4', 10 >= 12) # Valor esperado a retornar 'False', ya que 10 es menor que 12. 
+print('Prueba #5', 10 >= 10) # Valor esperado a retornar 'True', ya que 10 es igual que 10. 
+###########################################
+
+
+# El operador ' <= ', menor o igual que. 
+# Tambien se puede usar independiente ' < ', menor que.
+# Ejemplo practico 
+
+print('Prueba #6',  23 <= 21) # Valor esperado a retornar 'False', ya que 23 es mayor que 21. 
+print('Prueba #7', 10 <= 12) # Valor esperado a retornar 'True', ya que 10 es menor que 12. 
+print('Prueba #8', 10 <= 10) # Valor esperado a retornar 'True', ya que 10 es igual que 10. 
+###########################################
+
+
+
+
+
+
+
+
+
+
