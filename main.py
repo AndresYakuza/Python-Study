9cccf739e3e103189b04e09a17204a8eb30ef721
AndresYakuza <andresramos123@outlook.com>
2025-07-07 16:59:04 -0500

    Practica de ejercicios (Bucles - for) Reto

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
 
 
+import time
 
-# Verificar cuántos números son múltiplos de 3
-# Pide al usuario que ingrese 10 números. Muestra cuántos son múltiplos de 3.
+# Escribe un bucle for que cuente hasta cinco.
+    # Cuerpo del bucle: imprime el número de iteración del bucle y la palabra "Mississippi".
+    # Cuerpo del bucle, emplea : time.sleep(1)
 
-numeros = []
-multiplos_de_3 = []
+# Escribe una función print con el mensaje final.
 
-for i in range(1, 11):
-    while True:
-        try:
-            entrada = input(f'Número {i}: ')
-            numero = int(entrada)
-            numeros.append(numero)
+for i in range(1, 6):
+    print(i, 'Mississippi')
+    time.sleep(1)
 
-            if numero % 3 == 0:
-                multiplos_de_3.append(numero)
-            break
-        except ValueError:
-            print('Número no válido, intenta de nuevo...')
-
-print(f'\nNúmeros totales: {numeros}')
-print(f'Múltiplos de 3: {multiplos_de_3}')
-print(f'Cantidad de múltiplos de 3: {len(multiplos_de_3)}')
-
-
-
-
-
-
-
-    
-
-
-
-
-
-    
+print('¡Listos o no, ahí voy!')
